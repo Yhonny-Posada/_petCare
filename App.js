6f512e37375b600cf9cdce081175c3acef1f919f
yhonny <yhonnyalejandroposada@gmail.com>
2026-03-28 23:33:04 -0500

    Add data modules, navigation setup, and tips screen - Create pets and tips data sources - Implement AppNavigator for screen navigation - Add tips screen structure - Update App.js to integrate navigation and data

--- App.js
+++ App.js
@@ -3,4 +3,8 @@ import AppNavigator from './src/Navigation/AppNavigator';
 
 const App = () => <AppNavigator />;
 
+<<<<<<< Updated upstream
 export default App;
+=======
+export default App;
+>>>>>>> Stashed changes
---
+++ src/data/tipsData.js
@@ -0,0 +1,11 @@
+const tips =
+[
+  "Mantén hidratada a tu mascota",
+  "Visita regularmente al veterinario",
+  "Proporciona una dieta balanceada",
+  "Ejercicio diario es clave",
+  "Mantén su espacio limpio",
+  "Brinda cariño y atención",
+];
+
+export default tips;
